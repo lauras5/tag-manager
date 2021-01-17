0bec0cd27838dee5d805be2b3e4b358f0a76e17f
ALMaclaine <almaclaine@gmail.com>
2021-01-17 14:42:25 -0600

    add setup call

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const koa = require('koa')
 const Router = require('@koa/router')
 const bodyParser = require('koa-body');
-const {getTagScopedRouter} = require('sql-tag-system-koa-route');
+const {getTagScopedRouter, setupTagSystem} = require('sql-tag-system-koa-route');
 
 const dbInfo = {
   host: 'localhost',
   password: process.env.MYSQL_PW,
   database: 'sql_tag_system',
   user: 'root'
-}
+};
 
-const tagScopedRouter = getTagScopedRouter(dbInfo);
-const api = new Router();
-api.use(tagScopedRouter.routes())
-    .use(tagScopedRouter.allowedMethods())
+(async () => {
+  await setupTagSystem(dbInfo);
 
-const app = new koa()
-app.use(bodyParser({multipart: true}));
-const router = new Router()
+  const tagScopedRouter = getTagScopedRouter(dbInfo);
+  const api = new Router();
+  api.use(tagScopedRouter.routes())
+      .use(tagScopedRouter.allowedMethods())
 
-router.get('/', (ctx) => {
-  ctx.body = {};
-})
+  const app = new koa()
+  app.use(bodyParser({multipart: true}));
+  const router = new Router()
 
-app.use(router.routes())
-  .use(router.allowedMethods())
+  router.get('/', (ctx) => {
+    ctx.body = {};
+  })
 
-app.use(api.routes())
-    .use(api.allowedMethods())
+  app.use(router.routes())
+      .use(router.allowedMethods())
 
-app.listen(3000, () => console.log('running on port 3000'))
+  app.use(api.routes())
+      .use(api.allowedMethods())
+
+  app.listen(3000, () => console.log('running on port 3000'))
+})()
