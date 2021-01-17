43569226d048be94316a1581ced51028cddf9f46
ALMaclaine <almaclaine@gmail.com>
2021-01-17 14:40:08 -0600

    add basic fetch utils

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const koa = require('koa')
-const koaRouter = require('@koa/router')
+const Router = require('@koa/router')
+const bodyParser = require('koa-body');
+const {getTagScopedRouter} = require('sql-tag-system-koa-route');
+
+const dbInfo = {
+  host: 'localhost',
+  password: process.env.MYSQL_PW,
+  database: 'sql_tag_system',
+  user: 'root'
+}
+
+const tagScopedRouter = getTagScopedRouter(dbInfo);
+const api = new Router();
+api.use(tagScopedRouter.routes())
+    .use(tagScopedRouter.allowedMethods())
 
 const app = new koa()
-const router = new koaRouter()
+app.use(bodyParser({multipart: true}));
+const router = new Router()
 
 router.get('/', (ctx) => {
-  ctx.body = {
-    data: 'success'
-  };
+  ctx.body = {};
 })
 
 app.use(router.routes())
   .use(router.allowedMethods())
+
+app.use(api.routes())
+    .use(api.allowedMethods())
 
 app.listen(3000, () => console.log('running on port 3000'))
