62739fe0285b9a283fbf3bbb74f789609dec28e1
ALMaclaine <almaclaine@gmail.com>
2021-01-18 13:17:38 -0600

    use package fetch methods

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,26 +1,6 @@
 <script>
 	import {onMount} from 'svelte';
-
-	async function addTag(tag) {
-	    const res = await fetch(`/api/tag?tag=${tag}` , {method: 'post'});
-	    const {id} = await res.json();
-        return id;
-    }
-
-    async function getTag(id) {
-        const res = await fetch(`/api/tag?id=${id}`);
-        return await res.json();
-    }
-
-    async function listTags() {
-        const res = await fetch('/api/tag');
-        return await res.json();
-    }
-
-    async function deleteTag(id) {
-        const res = await fetch(`/api/tag?id=${id}`, {method: 'delete'});
-        return await res.json();
-    }
+	import {addTag, getTag, listTags, deleteTag} from 'sql-tag-system-koa-route';
 
 	onMount(async () => {
 
