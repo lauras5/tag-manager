d665c4d0b1301b16867656a8d3fac3ec9ef99c91
Lauras5 <idlaurasalcido@gmail.com>
2021-01-22 17:13:41 -0800

    loadbtn

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,6 +4,8 @@
 	import {addTag, getTag, listTags, deleteTag} from 'sql-tag-system-fetch-utils';
     let tags = [];
     let newTag = '';
+    let limit = 200;
+    let page = 0;
 
     async function removeTag(tagId) {
         try {
@@ -27,9 +29,16 @@
         }
     }
 
+    async function getNewPage() {
+        page++;
+
+        let additionalTags = await listTags(page, limit);
+        tags = [...tags, ...additionalTags];
+    }
+
 	onMount(async () => {
 	    try {
-            tags = await listTags();
+            tags = await listTags(page, limit);
         } catch (e) {
 	        console.log(`Error: ${e}`)
         }
@@ -38,15 +47,21 @@
 </script>
 
 <div>
-    <input class="new-tag-input" bind:value={newTag}>
-    <button class='pl-4 pr-4 bg-gray-200 rounded-2xl' on:click={handleAdd}>Add Tag</button>
-</div>
+    <div>
+        <input class="new-tag-input" bind:value={newTag}>
+        <button class='pl-4 pr-4 bg-gray-200 rounded-2xl' on:click={handleAdd}>Add Tag</button>
+    </div>
 
-<div class="tag-container flex flex-wrap">
-    {#each tags as {id, tag}}
-        <div transition:fade class='inline-flex m-2 bg-gray-200 p-1 pl-4 pr-1 rounded-2xl'>
-            <span>{tag}</span>
-            <span class="ml-4 bg-gray-300 rounded-full w-6 h-6 flex justify-center items-center leading-6 pb-0.5" on:click={removeTag(id)}>x</span>
-        </div>
-    {/each}
+    <div class="tag-container flex flex-wrap mt-3 mb-15 overflow-auto">
+        {#each tags as {id, tag}}
+            <div transition:fade class='inline-flex m-2 bg-gray-200 p-1 pl-4 pr-1 rounded-2xl'>
+                <span>{tag}</span>
+                <span class="ml-4 bg-gray-300 rounded-full w-6 h-6 flex justify-center items-center leading-6 pb-0.5" on:click={removeTag(id)}>x</span>
+            </div>
+        {/each}
+    </div>
+
+    <div class="fixed bottom-5 items-center w-screen flex justify-center items-center">
+        <button class="w-48 bg-blue-300 p-1 rounded-2xl" on:click={getNewPage}>Load More</button>
+    </div>
 </div>
