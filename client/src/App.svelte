e8924d20fed5beea6cf934bfa9342111bbd36f72
Lauras5 <idlaurasalcido@gmail.com>
2021-01-25 20:58:48 -0800

    issue 21

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,13 +2,13 @@
     import {tags, limit, page} from './stores.js';
     import {onMount} from 'svelte';
     import {listTags} from 'sql-tag-system-fetch-utils';
-    import Tag from './Tag.svelte';
-    import TagView from './TagView.svelte';
+    import TagComponent from './TagComponent.svelte';
 
     onMount(async () => {
         try {
+            console.log(await listTags($page, $limit))
             $tags = await listTags($page, $limit);
-        } catch(err) {
+        } catch (err) {
             console.log(err)
         }
     });
@@ -20,19 +20,9 @@
 
 </script>
 
-<div class="tag-container flex items-start content-start flex-wrap overflow-auto">
-    <TagView></TagView>
-    {#each $tags as tag}
-        <Tag {...tag}></Tag>
-    {/each}
-</div>
+<TagComponent></TagComponent>
 
 <div class="fixed bottom-5 items-center w-screen flex justify-center items-center">
     <button class="w-48 bg-blue-300 p-1 rounded-2xl focus:outline-none" on:click={handleLoadMore}>Load More</button>
 </div>
 
-<style>
-    .tag-container {
-        height: 100%;
-    }
-</style>
